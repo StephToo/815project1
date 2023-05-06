795476c552d5f2f46bc5869d111f783e22746cc6
Stephanie Toole <122947637+StephToo@users.noreply.github.com>
2023-05-06 11:06:00 -0500

    Add files via upload

---
+++ project1codeINPUT.py
@@ -0,0 +1,20 @@
+import random
+
+# Define the probability distribution with a configurable parameter
+# https://www.geeksforgeeks.org/random-normalvariate-function-in-python/
+def probability_distribution(parameter):
+    return random.normalvariate(0, parameter)
+
+# Set the configurable parameter values
+parameter_value_1 = 0.33
+parameter_value_2 = 0.15
+
+# Simulate the experiment and save the results to two different text files
+# https://www.pythontutorial.net/python-basics/python-write-text-file/
+with open('simulated_data_1.txt', 'w') as file1, open('simulated_data_2.txt', 'w') as file2:
+    for i in range(100):
+        result_1 = probability_distribution(parameter_value_1)
+        result_2 = probability_distribution(parameter_value_2)
+        file1.write(str(result_1) + '\n')
+        file2.write(str(result_2) + '\n')
+
