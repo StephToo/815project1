a3455b531b9478e11a9ad33f0d2734153d821db9
Stephanie Toole <122947637+StephToo@users.noreply.github.com>
2023-05-14 23:55:13 -0500

    Update project1codeINPUT.py

--- project1codeINPUT.py
+++ project1codeINPUT.py
@@ -1,20 +1,22 @@
-import random
+import matplotlib.pyplot as plt
 
-# Define the probability distribution with a configurable parameter
-# https://www.geeksforgeeks.org/random-normalvariate-function-in-python/
-def probability_distribution(parameter):
-    return random.normalvariate(0, parameter)
-
-# Set the configurable parameter values
-parameter_value_1 = 0.33
-parameter_value_2 = 0.15
-
-# Simulate the experiment, store simulated results in a persistent data format
+# Read in the data from the two text files
+# https://www3.ntu.edu.sg/home/ehchua/programming/webprogramming/Python_FileText.html
 # https://www.pythontutorial.net/python-basics/python-write-text-file/
-with open('simulated_data_1.txt', 'w') as file1, open('simulated_data_2.txt', 'w') as file2:
-    for i in range(100):
-        result_1 = probability_distribution(parameter_value_1)
-        result_2 = probability_distribution(parameter_value_2)
-        file1.write(str(result_1) + '\n')
-        file2.write(str(result_2) + '\n')
+with open('simulated_data_1.txt', 'r') as file1, open('simulated_data_2.txt', 'r') as file2:
+    data_1 = [float(line) for line in file1]
+    data_2 = [float(line) for line in file2]
+
+# Plot the data from the two scenarios on the same plot
+# https://www.geeksforgeeks.org/how-to-plot-data-from-a-text-file-using-matplotlib/
+plt.plot(data_1, label='Hydrogen Gas')
+plt.plot(data_2, label='Methane Gas')
+
+
+# Add labels and title to the plot
+plt.xlabel('Time (min)')
+plt.ylabel('Change in Flow Rate (sccm)')
+plt.title('Flow Rate of Gases Over Time in Chemical Vapor Deposition System')
+plt.legend()
+plt.show()
 
